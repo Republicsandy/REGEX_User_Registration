4b565e9e1d7e3c348c440561a3c9eb202073eea0
Sandeep singh <sandy180388@gmail.com>
2022-08-31 12:51:22 +0530

    [sandeep] First name file added in UC_1

---
+++ User_Registration_Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace User_Registration_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Welcome to Reg Expressions!");
+             Console.WriteLine("press 1 for first name");
+            var getValueByUser = Convert.ToInt32(Console.ReadLine());
+            switch (getValueByUser)
+            {
+                case 1:
+                    Console.WriteLine("Enter First Name :");
+                    string name = Console.ReadLine();
+                    Reg_Ex regex = new Reg_Ex();
+                    if (regex.ValidFirstName(name))
+                        Console.WriteLine("First Name is VALID !!");
+                    else
+                        Console.WriteLine("First Name is INVALID !!");
+                    break;
+            }
+           
+        }
+    }
+}
---
+++ User_Registration_Program/Reg_Ex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration_Program
+{
+    class Reg_Ex
+    {
+        //pattern for valid name
+        public static string REGEX = (@"^[A-Z][a-zA-Z]{2,}$");
+        public bool ValidFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX);
+        }
+    }
+}
